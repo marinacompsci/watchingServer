93ba5cca1256638bdfc9c51e5300f00d3f1c629e
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2020-10-30 20:44:32 +0100

    getting key from url params

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 let express = require('express');
 let app = express();
+let location;
 
-app.get('/', (req, res) => {
-  res.send('Responded');
+
+app.get('/locations/:key', (req, res) => {
+  //console.log(req.params)
+  location = JSON.stringify(new Location(req.params.key, 1211, 2222, 18012020));
+  res.contentType('json');
+  res.send(location);
 });
 
 app.listen(5500, 'localhost', () => {
   console.log("Server running...");
-
 });
+
+function Location(id, longitude, latitude, time) {
+  this.id = id;
+  this.longitude = longitude;
+  this.latitude = latitude;
+  this.time = time;
+}
+
