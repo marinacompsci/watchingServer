3a0b24954acd3b19f37aa26c6997d45876f9066f
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2020-11-24 13:35:31 +0100

    deleted some comments

--- server.js
+++ server.js
@@ -2,21 +2,8 @@ let express = require('express');
 let app = express();
 var bodyParser = require('body-parser');  
 let location;
-let connectedToDB = false;
 var urlencodedParser = bodyParser.urlencoded({ extended: false })  
 let data = new Map();
-
-// DB
-/*
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-});
-
-client.connect()
-.then(() => connectedToDB = true);
-*/
 
 //app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -34,25 +21,6 @@ app.get('/locations/:id', (req, res) => {
     res.status(404).send('Error: Nothing found under the given key');
   }
   
-  /*if (connectedToDB) {
-    res.contentType('json');
-    let query = `SELECT * FROM locations WHERE id = ${req.params.id};`;
-    client.query(query, (err, dbRes) => {
-      if (err) {
-        console.log(JSON.stringify(err)); 
-        res.send(JSON.stringify(err));
-      } else {
-        if (dbRes.rowCount != 0) { // If there is actually a DB entry with this ID
-          let jsonResponse = JSON.stringify(new Location(dbRes.rows[0].id, dbRes.rows[0].long, dbRes.rows[0].lat, dbRes.rows[0].modtime));
-          res.send(jsonResponse);
-        } else {
-          res.send("No entry on DB with this ID.");
-        }
-      }
-    });
-  } else {
-    res.send("Connection to DB NOT established.");
-  }*/
 });
 
 app.post('/locations', urlencodedParser, (req, res) => {
@@ -60,13 +28,6 @@ app.post('/locations', urlencodedParser, (req, res) => {
   location = new Location(req.body.id, req.body.long, req.body.lat);
   data.set(location.id, location);
   res.contentType('json');
-  
-  /*if (connectedToDB) {
-    tableHasEntry(res, location);
-  } else {
-    res.send("Connection to DB NOT established.");
-  }*/
-
   res.send(data.get(location.id)); 
 });
 
@@ -81,42 +42,3 @@ function Location(id, longitude, latitude, time) {
   this.latitude = latitude;
   this.time = time;
 }
-
-
-/*
-function tableHasEntry(res, location) {
-  let query = `SELECT * FROM locations WHERE id = ${location.id}`;
-  client.query(query, (err, dbRes) => {
-    if (err) {
-      client.end()
-      .then(() => {
-        res.send(err + "\n" + "Error while selecting table entries with passed ID");
-      })
-      return;
-    } else if (dbRes.rowCount == 0){
-      console.log("Trying to insert new table entry");
-      query = `INSERT INTO locations (id,long,lat) VALUES (${location.id}, ${location.longitude}, ${location.latitude});`;
-        client.query(query, (err, dbRes) => {
-          if (err) {
-            console.log(JSON.stringify(err)); 
-            res.send(JSON.stringify(err));
-          } else {
-            console.log("Successfully inserted new location.");
-            res.send(location);
-          }
-        });
-    } else {       // A table entry with this ID already exists
-      console.log("Trying to update existing table entry");
-      query = `UPDATE locations SET (long,lat) = (${location.longitude}, ${location.latitude}) WHERE id = ${location.id};`; 
-      client.query(query, (err, dbRes) => {
-          if(err) {
-            console.log(JSON.stringify(err)); 
-            res.send(err);
-          } else {
-            console.log("Successfully updated existing location.");
-            res.send("Successfully updated existing location.");
-          }
-      });
-    }
-  });
-}*/
